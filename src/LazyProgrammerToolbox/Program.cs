cc1ce1af57c08de293343a1acf33a860447c46ed
Francesco Diana <frdiana@microsoft.com>
2020-04-05 19:10:39 +0200

    Console App project created

---
+++ src/LazyProgrammerToolbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LazyProgrammerToolbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
