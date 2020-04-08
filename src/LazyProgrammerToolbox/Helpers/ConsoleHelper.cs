afc3219e1d958688eb99a92c6ec8f2b4f72252e9
Francesco Diana <frdiana@microsoft.com>
2020-04-09 00:22:45 +0200

    implemented toAzSettings command

---
+++ src/LazyProgrammerToolbox/Helpers/ConsoleHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyProgrammerToolbox.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void WriteSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ src/LazyProgrammerToolbox/Models/AzureSettingsJsonFormat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyProgrammerToolbox.Models
+{
+    public class AzureSettingsJsonFormat
+    {
+        public List<AzureSettingsJsonFormatItem> Properties { get; set; } = new List<AzureSettingsJsonFormatItem>();
+    }
+
+    public class AzureSettingsJsonFormatItem
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool SlotSetting { get; set; }
+    }
+}
