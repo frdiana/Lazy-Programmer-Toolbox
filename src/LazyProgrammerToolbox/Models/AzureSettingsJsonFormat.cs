eea000debc663398b292bd2a49e66ae2b662c703
Francesco Diana <frdiana@microsoft.com>
2020-05-19 12:56:59 +0200

    fixes: #7 #8

--- src/LazyProgrammerToolbox/Models/AzureSettingsJsonFormat.cs
+++ src/LazyProgrammerToolbox/Models/AzureSettingsJsonFormat.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace LazyProgrammerToolbox.Models
 {
+   
     /// <summary>
-    /// The poco class for serialize Azure app service settings
-    /// </summary>
-    public class AzureSettingsJsonFormat
-    {
-        public List<AzureSettingsJsonFormatItem> Properties { get; set; } = new List<AzureSettingsJsonFormatItem>();
-    }
-
-    /// <summary>
-    /// Rapresents a single azure config node.
+    /// Rapresents a single azure config element.
     /// </summary>
     public class AzureSettingsJsonFormatItem
     {
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("value")]
         public string Value { get; set; }
+        [JsonPropertyName("slotSetting")]
         public bool SlotSetting { get; set; }
     }
 }
